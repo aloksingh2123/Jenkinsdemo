50b3bbee382047e6454563260c42abb5e91b183a
aloksingh2123 <aloksingh2123@gmail.com>
2021-07-29 10:06:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jenkinsdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
